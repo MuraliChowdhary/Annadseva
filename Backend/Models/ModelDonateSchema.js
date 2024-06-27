3562be942fa588178d57ded014f03d9eb7d8d474
KovuriVaishnavi <22B81A6762@CVR.AC.IN>
2024-06-27 14:02:59 +0530

    commit

--- Backend/Models/ModelDonateSchema.js
+++ Backend/Models/ModelDonateSchema.js
@@ -1,1 +1,6 @@
 const mongoose=require('mongoose');
+const donateSchema=new mongoose.Schema({
+    
+})
+const donateModel=mongoose.model('donate',donateSchema);
+module.exports=donateModel;
--- Backend/Models/ModelRequestSchema.js
+++ Backend/Models/ModelRequestSchema.js
@@ -1,1 +1,6 @@
 const mongoose=require('mongoose');
+const requestSchema=new mongoose.Schema({
+    
+})
+const requestModel=mongoose.model('request',requestSchema);
+module.exports=requestModel;
