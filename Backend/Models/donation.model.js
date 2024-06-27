6f86fcc464091cc0f475325bdb9e73e713fae711
Yashwanth0620 <yashwanth.k0620@gmail.com>
2024-06-27 14:35:56 +0530

    complete schema

---
+++ Backend/Models/donation.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const donationSchema = new mongoose.Schema(
+  {
+    donorId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    loc: { type: String, required: true },
+    donarName: { type: String, required: true },
+    foodItems: [{ type: String, required: true }],
+    quantity: { type: Number, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "completed"],
+      default: "pending",
+    },
+    shelfLife: { type: Number, required: true },
+    receiverId: { type: Schema.Types.ObjectId, ref: "User" },
+    misc: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Donation", donationSchema);
