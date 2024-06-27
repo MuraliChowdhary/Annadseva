eecd88ac40c5e690b6f8d56ee80fb3bf9b7c749a
KovuriVaishnavi <22B81A6762@CVR.AC.IN>
2024-06-27 13:07:02 +0530

    commit

--- Backend/Routes/RoutesUser.js
+++ Backend/Routes/RoutesUser.js
@@ -1,6 +1,8 @@
 const express=require('express');
-const userController=require('../controllers/UserController');
+
 const routes=express.Router();
+
+const userController=require('../controllers/UserController');
 routes.post('/user/signup',userController.SignUpuser)
 routes.post('/user/login',userController.LoginUser)
 module.exports=routes;
--- Backend/index.js
+++ Backend/index.js
@@ -5,13 +5,33 @@ const app=express();
 app.listen(PORT,()=>{
     console.log(`server running at ${PORT}`)
 })
+
+
 app.use(express.json())
 const cors=require('cors')
 app.use(cors())
-//user routes
+
+
+
+
+//Routes
 const userRoutes=require('./Routes/RoutesUser');
 app.use('/api',userRoutes)
 
+
+const adminRoutes=require('./Routes/RoutesAdmin');
+app.use('/api',adminRoutes);
+
+
+const donateRoutes=require('./Routes/RoutesDonate');
+app.use('/api',donateRoutes);
+
+
+const requestRoutes=require('./Routes/RoutesRequest');
+app.use('/api',requestRoutes);
+
+const volunteerRoutes=require('./Routes/RoutesVolunteer');
+app.use('/api',volunteerRoutes);
 
 
 
