371c290bacb886c2901aa3a818be8318451e846d
Nikhitha <nikhithamadhagoni@gmail.com>
2024-06-28 17:28:26 +0530

    logs complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import './App.css';
 import UserRegistration from './components/userRegistration';
 import UserTypeSelection from './components/UserTypeSelection';
 import VolunteerDashboard from './components/VolunteerDashBoard/volunteerDashboard';
+import Log from './components/Logs/log';
 
 const App = () => {
   return (
@@ -19,15 +20,16 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-    
-    
-          <Route path="/aboutus" element={<Aboutus/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<UserRegistration/>} />
-          <Route path="/admin" element={<AdminDashboard/>} />
+
+
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<UserRegistration />} />
+          <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/user-type-selection" element={<UserTypeSelection />} />
           <Route path="/volunteer/dashboard" element={<VolunteerDashboard />} />
+          <Route path="/log" element={<Log />} />
         </Routes>
       </div>
     </BrowserRouter>
