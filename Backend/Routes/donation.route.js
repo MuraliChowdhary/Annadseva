bd3b8cb78b9a570c1f78f6290a6e27e8d4a5292a
KovuriVaishnavi <22B81A6762@CVR.AC.IN>
2024-06-28 10:32:56 +0530

    commit

--- Backend/Routes/donation.route.js
+++ Backend/Routes/donation.route.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const {
-  postDonation
-} = require("../controllers/donation.controller");
+// const {
+//   postDonation
+// } = require("../controllers/donation.controller");
 const { adminAuth } = require("../middleware/adminAuth");
 const router = express.Router();
 
-router.use("/donations").post(postDonation);
+// router.use("/donations").post(postDonation);
 
 module.exports = router;
