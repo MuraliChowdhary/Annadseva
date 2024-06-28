e45aa627b4e51df1e2d1f018a4b11d7e29ea3b7d
Yashwanth0620 <yashwanth.k0620@gmail.com>
2024-06-28 10:44:56 +0530

    merge-1

--- Backend/controllers/admin.controller.js
+++ Backend/controllers/admin.controller.js
@@ -4,5 +4,22 @@ const User = require("../Models/user.model");
 const Requests = require("../Models/request.model");
 
 const getAllVolunteers = errorHandler(async (req, res) => {
-    
+    const users = await User.find();
+    res.status(200).json(users);
 })
+
+const getAllDonations = errorHandler(async (req, res) => {
+    const donations = await Donation.find();
+    res.status(200).json(donations);
+})
+
+const getAllRequests = errorHandler(async (req, res) => {
+    const requests = await Requests.find();
+    res.status(200).json(requests);
+})
+
+module.exports = {
+    getAllDonations, 
+    getAllRequests,
+    getAllVolunteers
+}
