ff7ed3e13e8d3e6723dbcfa024ef1b8b7b56820c
RajeshAvadootha <aitoolsrajesh@proton.me>
2024-06-28 11:13:54 +0530

    merge3/rajesh

--- Backend/Models/transaction.model.js
+++ Backend/Models/transaction.model.js
@@ -1,11 +1,24 @@
 const mongoose = require("mongoose");
 
-const transactionSchema = new mongoose.Schema({
-    donorId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    receiverId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    volunteerId: { type: Schema.Types.ObjectId, ref: 'User' },
-    donationId: { type: Schema.Types.ObjectId, ref: 'Donation', required: true },
-    status: { type: String, enum: ['pending', 'accepted', 'completed'], default: 'pending' }
-}, { timestamps: true });
+const transactionSchema = new mongoose.Schema(
+  {
+    donorName: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    receiverName: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    volunteerId: { type: Schema.Types.ObjectId, ref: "User" },
+    donationId: {
+      type: Schema.Types.ObjectId,
+      ref: "Donation",
+      required: true,
+    },
+    rloc: { type: String, required: true },
+    dloc: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "failed", "completed"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Transaction', transactionSchema);
+module.exports = mongoose.model("Transaction", transactionSchema);
---
+++ Backend/statusCodes.js
@@ -0,0 +1,11 @@
+module.constants = {
+  SUCCESS: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  SERVER_ERROR: 500,
+};
+
+module.exports = constants;
