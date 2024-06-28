189e5617d6e7b9a82517a1e310eff117123c5d12
RajeshAvadootha <aitoolsrajesh@proton.me>
2024-06-28 12:28:56 +0530

    rdmcmt/rajesh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from './components/Header';
-import Home from './components/Home';
-import Contact from './components/Contact';
-import Aboutus from './components/Aboutus';
-import AdminDashboard from './components/Admindashboard';
-import Login from './components/Login';
-import Register from './components/Register';
-import './App.css';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Aboutus from "./components/Aboutus";
+import AdminDashboard from "./components/Admindashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import "./App.css";
+import VolunteerDashboard from "./components/VolunteerDashboard";
 
 const App = () => {
   return (
@@ -16,13 +17,13 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-    
-    
-          <Route path="/aboutus" element={<Aboutus/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Register/>} />
-          <Route path="/admin" element={<AdminDashboard/>} />
+
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/volunteer/dashboard" element={<VolunteerDashboard />} />
         </Routes>
       </div>
     </BrowserRouter>
