b2a20d76cce19f6c6aac0da83b4eedf39059750b
Yashwanth0620 <yashwanth.k0620@gmail.com>
2024-06-28 10:10:51 +0530

    random

--- Backend/Routes/donation.route.js
+++ Backend/Routes/donation.route.js
@@ -2,11 +2,8 @@ const express = require("express");
 const {
   postDonation
 } = require("../controllers/donation.controller");
-const { adminAuth } = require("../middleware/adminAuth");
 const router = express.Router();
 
-// Admin Routes
-router.use(adminAuth);
 router.use("/donations").post(postDonation);
 
 module.exports = router;
---
+++ Backend/controllers/admin.controller.js
@@ -0,0 +1,8 @@
+const errorHandler = require("express-async-handler");
+const Donation = require("../Models/donation.model");
+const User = require("../Models/user.model");
+const Requests = require("../Models/request.model");
+
+const getAllVolunteers = errorHandler(async (req, res) => {
+    
+})
